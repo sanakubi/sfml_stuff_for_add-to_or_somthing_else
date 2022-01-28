27d600292b7fc3b8a7a66a312072abe47015044d
Kyoko <44459344+IWantToDiePlsHelpMe@users.noreply.github.com>
2022-01-29 03:54:48 +0500

    Поворот вектора
    Работает СТРАННО

---
+++ VectorRotationTest.cpp
@@ -0,0 +1,43 @@
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <iostream>
+
+using namespace sf;
+
+int main() {
+    RenderWindow window(VideoMode(500, 500), "- - -");
+
+    RectangleShape strelka(Vector2f(5, 225));
+    CircleShape crc;
+    Vector2f delta;
+
+    crc.setPosition(400, 400);
+    crc.setFillColor(Color(30, 144, 255, 255));
+    crc.setRadius(10);
+
+    strelka.setPosition(250, 250);
+    strelka.setFillColor(Color(255, 0, 0));
+
+    delta = strelka.getPosition() - crc.getPosition();
+    
+    float ratio = atan2(delta.y, delta.x); 
+    
+    std::cout << ratio << " " << ratio * (180.0 / 3.14) << std::endl;
+    
+    strelka.setRotation(90 + ratio * (180.0 / 3.14)); 
+    
+
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+        window.draw(strelka);
+        window.draw(crc);
+        window.display();
+        window.clear();
+    }
+    return 0;
+}
