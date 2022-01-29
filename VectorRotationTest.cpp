9c3e7efa3785a197b7c3900501abe8035f8758e6
Kyoko <44459344+IWantToDiePlsHelpMe@users.noreply.github.com>
2022-01-30 01:59:13 +0500

    Mouse event
    Add mouse event: The vector follows the mouse.

--- VectorRotationTest.cpp
+++ VectorRotationTest.cpp
@@ -4,36 +4,54 @@
 
 using namespace sf;
 
+void ratio_upd(CircleShape& crc, RectangleShape& strel) {
+    Vector2f delta;
+
+    delta = strel.getPosition() - crc.getPosition();
+    float ratio = atan2(delta.y - 11, delta.x - 11);
+    std::cout << ratio << " " << ratio * (180.0 / 3.14) << std::endl;
+    strel.setRotation(90 + ratio * (180.0 / 3.14));
+}
+
 int main() {
+    int W, H;
+    W = 1; H = 1;
+
     RenderWindow window(VideoMode(500, 500), "- - -");
 
     RectangleShape strelka(Vector2f(2, 225));
     CircleShape crc;
     Vector2f delta;
 
-    crc.setPosition(400, 400);
+    crc.setPosition(W, H);
     crc.setFillColor(Color(30, 144, 255, 255));
     crc.setRadius(10);
 
     strelka.setPosition(250, 250);
     strelka.setFillColor(Color(255, 0, 0));
-
-    delta = strelka.getPosition() - crc.getPosition();
- 
-    float ratio = atan2(delta.y - 11, delta.x - 11); 
     
-    std::cout << ratio << " " << ratio * (180.0 / 3.14) << std::endl;
+    ratio_upd(crc, strelka);
     
-    strelka.setRotation(90 + ratio * (180.0 / 3.14));
-    
-
 
     while (window.isOpen()) {
         Event event;
+        
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
+            if (event.type == Event::MouseMoved)
+            {
+                std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
+                std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
+
+                W = event.mouseMove.x; 
+                H = event.mouseMove.y;
+
+                crc.setPosition(W, H);              
+                ratio_upd(crc, strelka);
+            }
         }
+        
         window.draw(strelka);
         window.draw(crc);
         window.display();
