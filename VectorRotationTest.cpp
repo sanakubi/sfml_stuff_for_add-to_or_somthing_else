5265f4f356059e8d3d3eacf53620a10225dfed2f
Kyoko <44459344+IWantToDiePlsHelpMe@users.noreply.github.com>
2022-02-01 21:00:06 +0500

    Nothing
    Оказывается, лагает из-за потоковых выводов в консоль (cout). Удалив их, я получил больше фепесов

--- VectorRotationTest.cpp
+++ VectorRotationTest.cpp
@@ -6,19 +6,17 @@ using namespace sf;
 
 void ratio_upd(CircleShape& crc, RectangleShape& strel) {
     Vector2f delta;
-
     delta = strel.getPosition() - crc.getPosition();
     float ratio = atan2(delta.y - 11, delta.x - 11);
-    std::cout << ratio << " " << ratio * (180.0 / 3.14) << std::endl;
     strel.setRotation(90 + ratio * (180.0 / 3.14));
 }
 
 void strelkpos(Event& event, RectangleShape& strel) {
     switch (event.key.code) {
-    case Keyboard::Up: strel.move(0, -1); break;
-    case Keyboard::Down: strel.move(0, 1); break;
-    case Keyboard::Right: strel.move(1, 0); break;
-    case Keyboard::Left: strel.move(-1, 0); break;
+        case Keyboard::Up: strel.move(0, -1); break;
+        case Keyboard::Down: strel.move(0, 1); break;
+        case Keyboard::Right: strel.move(1, 0); break;
+        case Keyboard::Left: strel.move(-1, 0); break;
     }
 }
 
@@ -46,9 +44,8 @@ int main() {
 
             if (event.type == Event::Closed)
                 window.close();
+
             if (event.type == Event::MouseMoved) {
-                std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
-                std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
 
                 crc.setPosition(event.mouseMove.x - 11, event.mouseMove.y - 11);
                 ratio_upd(crc, strelka);
@@ -58,6 +55,7 @@ int main() {
                 if (x >= 999) x = 0;
                 x++;
             }
+
             if (event.type == Event::KeyPressed) {
                 strelkpos(event, strelka);
                 ratio_upd(crc, strelka);
@@ -70,5 +68,6 @@ int main() {
         window.draw(crc);
         window.display();
     }
+
     return 0;
 }
